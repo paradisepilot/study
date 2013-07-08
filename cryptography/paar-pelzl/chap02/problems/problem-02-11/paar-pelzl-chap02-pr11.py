414d211b249ee5f3f4be8f412a42dc0bbbd90d83
Kenneth Chu <paradisepilot@gmail.com>
2013-07-07 20:54:42 -0400

    updated: paar-pelzl-chap02-pr11.py

--- cryptography/paar-pelzl/chap02/problems/problem-02-11/paar-pelzl-chap02-pr11.py
+++ cryptography/paar-pelzl/chap02/problems/problem-02-11/paar-pelzl-chap02-pr11.py
@@ -11,29 +11,4 @@ period          = 6
 
 plaintext = y.decrypt(ciphertext,plaintextPrefix,period)
 print('plaintext: ' + plaintext );
-
-exit();
-
-######################################################################
-print("\ny.getBit('Z')")
-print(   y.getBit('Z') )
-
-print("\ny.getBit('5')")
-print(   y.getBit('5') )
-
-print("\ny.getChar(24)")
-print(   y.getChar(24) )
-
-print("\ny.getChar(25)")
-print(   y.getChar(25) )
-
-print("\ny.getChar(26)")
-print(   y.getChar(26) )
-
-print("\ny.getChar(27)")
-print(   y.getChar(27) )
-
-print("\ny.getChar(28)")
-print(   y.getChar(28) )
-
 print('')
