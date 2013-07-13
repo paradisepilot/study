9b3f008be0028db5208f7c28e80a7f9469ffe7cb
Kenneth Chu <paradisepilot@gmail.com>
2013-07-13 14:36:00 -0400

    updated: DES.py

--- cryptography/buchmann/chap05/examples/example-05-03/DES.py
+++ cryptography/buchmann/chap05/examples/example-05-03/DES.py
@@ -47,6 +47,32 @@ class DES:
 			28,29,30,31,32, 1
 			]
 
+		self.FeistelPC1Vector = [
+			57,49,41,33,25,17, 9,
+			 1,58,50,42,34,26,18,
+			10, 2,59,51,43,35,27,
+			19,11, 3,60,52,44,36,
+			63,55,47,39,31,23,15,
+			 7,62,54,46,38,30,22,
+			14, 6,61,53,45,37,29,
+			21,13, 5,28,20,12, 4
+			]
+
+		self.FeistelPC2Vector = [
+			14,17,11,24, 1, 5, 3,28,
+			15, 6,21,10,23,19,12, 4,
+			26, 8,16, 7,27,20,13, 2,
+			41,52,31,37,47,55,30,40,
+			51,45,33,48,44,49,39,56,
+			34,53,46,42,50,36,29,32
+			]
+
+		self.leftShiftVector = [2] * self.numOfRounds
+		self.leftShiftVector[ 0] = 1
+		self.leftShiftVector[ 1] = 1
+		self.leftShiftVector[ 8] = 1
+		self.leftShiftVector[15] = 1
+
 		self.bitStrings = dict()
 		for i in range(0,len(self.alphabet)):
 			self.bitStrings[self.alphabet[i]] = bin(i).replace('0b','').zfill(self.numOfBits)
@@ -99,50 +125,75 @@ class DES:
 	def mod2(self,x):
 		return(int(round(x)) % 2)
 
-	def getFeedbackCoefficients(self,keystream,period):
-		myLHS    = self.toColumnVector(keystream[period:2*period])
-		myMatrix = self.getCoefficientMatrix(keystream,period)
-		soln     = map(self.mod2,linalg.solve(myMatrix,myLHS))
-		return(soln)
+	#def getFeedbackCoefficients(self,keystream,period):
+	#	myLHS    = self.toColumnVector(keystream[period:2*period])
+	#	myMatrix = self.getCoefficientMatrix(keystream,period)
+	#	soln     = map(self.mod2,linalg.solve(myMatrix,myLHS))
+	#	return(soln)
 
-	def getKeyStream(self,initialKeyStream,feedbackCoefficients,length):
-		period    = len(initialKeyStream)
-		keystream = [-1] * length
-		keystream[0:period] = map(int,list(initialKeyStream))
-		for i in range(period,len(keystream)):
-			keystream[i] = sum(keystream[(i-period):i] * array(feedbackCoefficients)) % 2
-		return(''.join(map(str,keystream)))
+	#def getKeyStream(self,initialKeyStream,feedbackCoefficients,length):
+	#	period    = len(initialKeyStream)
+	#	keystream = [-1] * length
+	#	keystream[0:period] = map(int,list(initialKeyStream))
+	#	for i in range(period,len(keystream)):
+	#		keystream[i] = sum(keystream[(i-period):i] * array(feedbackCoefficients)) % 2
+	#	return(''.join(map(str,keystream)))
 
 	def encrypt(self,plaintext,key):
-		plainbits = self.text2bits(plaintext)
+		plainbits    = self.text2bits(plaintext)
+		roundKeys    = self.getRoundKeys(key,self.leftShiftVector)
+		permutedBits = self.initialPermutation(plainbits)
+		cipherbits   = self.FeistelCipher(permutedBits,roundKeys)
 		print('plainbits')
 		print( plainbits )
-		permutedBits = self.initialPermutation(plainbits)
 		print('permutedBits')
 		print( permutedBits )
-		cipherbits = self.FeistelCipher(permutedBits)
 		print('cipherbits')
 		print( cipherbits )
+		#keybits = self.text2bits(key)
+		#print('keybits');
+		#print( keybits );
+		for i in range(0,len(roundKeys)):
+			print('i = ' + str(i) + ', roundKey = ' + roundKeys[i])
 		return(plainbits)
 
+	def getRoundKeys(self,key,leftShiftVector):
+		tempCD     = self.FeistelPC1(self.text2bits(key))
+		tempLength = len(tempCD)/2
+		roundKeys = [-1]*self.numOfRounds
+		for i in range(0,self.numOfRounds):
+			tempC  = [tempCD[j] for j in range(0,tempLength)]
+			tempD  = [tempCD[j] for j in range(tempLength,2*tempLength)]
+			tempC  = [tempC[(j+leftShiftVector[i]) % tempLength] for j in range(0,tempLength)]
+			tempD  = [tempD[(j+leftShiftVector[i]) % tempLength] for j in range(0,tempLength)]
+			tempCD = tempC + tempD
+			roundKeys[i] = self.FeistelPC2(''.join(tempCD))
+		return(roundKeys)
+
 	def initialPermutation(self,bitString):
-		temp = [bitString[i-1] for i in self.initialPermutationVector]
-		return(''.join(temp))
+		return(''.join([bitString[i-1] for i in self.initialPermutationVector]))
+
+	def FeistelPC1(self,bitString):
+		return(''.join([bitString[i-1] for i in self.FeistelPC1Vector]))
+
+	def FeistelPC2(self,bitString):
+		return(''.join([bitString[i-1] for i in self.FeistelPC2Vector]))
 
 	def FeistelExpand(self,bitString):
-		temp = [bitString[i-1] for i in self.FeistelExpansionVector]
-		return(''.join(temp))
+		return(''.join([bitString[i-1] for i in self.FeistelExpansionVector]))
 
-	def FeistelEncrypt(self,bitString,roundIndex):
-		self.FeistelExpand(bitString)
+	def FeistelEncrypt(self,bitString,roundKey):
+		temp = self.addBits(self.FeistelExpand(bitString),roundKey)
+		print('FeistelEncrypt')
+		print( temp )
 		return(bitString)
 
-	def FeistelCipher(self,bitString):
+	def FeistelCipher(self,bitString,roundKeys):
 		tempL = ''.join([bitString[i] for i in range(0,len(bitString)/2)])
 		tempR = ''.join([bitString[i] for i in range(len(bitString)/2,len(bitString))])
 		for roundIndex in range(0,self.numOfRounds):
 			tempL1 = tempL
 			tempL  = tempR
-			tempR  = self.addBits(tempL1,self.FeistelEncrypt(tempR,roundIndex))
+			tempR  = self.addBits(tempL1,self.FeistelEncrypt(tempR,roundKeys[roundIndex]))
 		return(tempR + tempL)
 
