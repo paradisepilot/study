c3074fa706affb9d0da8bdbe660c3f5df01dc6cc
Kenneth Chu <paradisepilot@gmail.com>
2013-08-12 12:42:31 -0400

    added: exercise-08-02/TrialDivision.R

---
+++ cryptography/buchmann/chap08/exercises/exercise-08-02/TrialDivision.R
@@ -0,0 +1,15 @@
+
+TrialDivision <- function(n = NULL, list.of.primes = NULL) {
+	
+	LIST.output <- list(
+		n              = n,
+		list.of.primes = list.of.primes,
+		prime.factors  = c(),
+		exponents      = c(),
+		unfactored     = 1
+		);
+
+	return(LIST.output);
+
+	}
+
