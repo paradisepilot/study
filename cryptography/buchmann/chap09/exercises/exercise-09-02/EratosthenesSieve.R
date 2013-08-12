b90781c03c36938e95e4139f15b5f9e9bf9f21bb
Kenneth Chu <paradisepilot@gmail.com>
2013-08-12 11:01:53 -0400

    updated: EratosthenesSieve.R

--- cryptography/buchmann/chap09/exercises/exercise-09-02/EratosthenesSieve.R
+++ cryptography/buchmann/chap09/exercises/exercise-09-02/EratosthenesSieve.R
@@ -1,17 +1,18 @@
 
 EratosthenesSieve <- function(upper.bound = NULL) {
-	
+
 	limit         <- ceiling(sqrt(upper.bound));
 	remaining     <- 2:upper.bound;
 	current.prime <- 2;
 	primes        <- c();
+
 	while (current.prime < limit) {
-		primes <- c(primes,current.prime);
-		remaining <- setdiff(remaining,current.prime*seq(1:upper.bound));
+		primes        <- c(primes,current.prime);
+		remaining     <- setdiff(remaining,current.prime*seq(1:upper.bound));
 		current.prime <- min(setdiff(remaining,primes));
 		}
 
 	return(remaining);
-	
+
 	}
 
