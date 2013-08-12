357458a9d0089eca7a9bb1136c9470063c39ba21
Kenneth Chu <paradisepilot@gmail.com>
2013-08-12 13:27:13 -0400

    updated: TrialDivision.R and test_EratosthenesSieve.R

--- cryptography/buchmann/chap08/exercises/exercise-08-02/TrialDivision.R
+++ cryptography/buchmann/chap08/exercises/exercise-08-02/TrialDivision.R
@@ -1,11 +1,31 @@
 
 TrialDivision <- function(n = NULL, list.of.primes = NULL) {
-	
+
+	prime.factors <- c();
+	exponents     <- c();
+
+	index      <- 0;
+	unfactored <- n;
+	while (unfactored > 1 & index < length(list.of.primes)) {
+		index <- 1 + index;
+		current.prime = list.of.primes[index];
+		if (0 == (unfactored %% current.prime)) {
+			prime.factors    <- c(prime.factors,current.prime);
+			unfactored       <- unfactored / current.prime;
+			current.exponent <- 1;
+			while (0 == unfactored %% current.prime) {
+				current.exponent <- 1 + current.exponent;
+				unfactored       <- unfactored / current.prime;
+				}
+			exponents <- c(exponents,current.exponent);
+			}
+		}
+
 	LIST.output <- list(
 		n              = n,
 		list.of.primes = list.of.primes,
-		prime.factors  = c(),
-		exponents      = c(),
+		prime.factors  = prime.factors,
+		exponents      = exponents,
 		unfactored     = 1
 		);
 
--- cryptography/buchmann/chap08/exercises/exercise-08-02/test_EratosthenesSieve.R
+++ cryptography/buchmann/chap08/exercises/exercise-08-02/test_EratosthenesSieve.R
@@ -1,5 +1,14 @@
 
 source("EratosthenesSieve.R");
+source("TrialDivision.R");
 
-EratosthenesSieve(upper.bound = 100);
+n <- 831802500;
+
+primes <- EratosthenesSieve(upper.bound = ceiling(sqrt(n)));
+
+results <- TrialDivision(n = n, list.of.primes = primes)
+print( results$prime.factors );
+print( results$exponents );
+
+prod(results$prime.factors ^ results$exponents);
 
