17255de6805a807e8201e3be10e872ade49db545
Kenneth Chu <paradisepilot@gmail.com>
2013-08-12 19:04:24 -0400

    updated: buchmann-exercise-09-03.R

--- cryptography/buchmann/chap09/exercises/exercise-09-03/buchmann-exercise-09-03.R
+++ cryptography/buchmann/chap09/exercises/exercise-09-03/buchmann-exercise-09-03.R
@@ -26,3 +26,16 @@ print( results$unfactored );
 
 results$unfactored * prod(results$prime.factors ^ results$exponents);
 
+####################################################################################################
+n <- results$unfactored - 1;
+print( n );
+
+primes <- EratosthenesSieve(upper.bound = ceiling(sqrt(n)));
+
+results <- TrialDivision(n = n, list.of.primes = primes)
+print( results$prime.factors );
+print( results$exponents );
+print( results$unfactored );
+
+results$unfactored * prod(results$prime.factors ^ results$exponents);
+
