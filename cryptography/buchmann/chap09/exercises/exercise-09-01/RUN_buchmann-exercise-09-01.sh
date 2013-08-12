7c15a8b01f3d6d7eaa74dce3c7dd6d6363d913b4
paradisepilot <paradisepilot@gmail.com>
2013-08-12 01:20:44 -0400

    added chap09/

---
+++ cryptography/buchmann/chap09/exercises/exercise-09-01/RUN_buchmann-exercise-09-01.sh
@@ -0,0 +1,15 @@
+
+outputDIR=output
+if [ -d ${outputDIR} ]; then
+	echo "The directory ${outputDIR} already exists. Program aborted."
+	exit
+else
+	mkdir ${outputDIR}
+fi
+
+cd ${outputDIR}
+
+myRscript=buchmann-exercise-09-01.R
+outputFILE=stdout.R.`basename ${myRscript} .R`
+R --no-save < ../${myRscript} 2>&1 > ${outputFILE}
+
