df899c3120091219874bf0a92cee58f2cfc51925
Kenneth Chu <paradisepilot@gmail.com>
2013-08-13 12:27:06 -0400

    updated: pMinusOne.R

--- cryptography/buchmann/chap09/code/pMinusOne.R
+++ cryptography/buchmann/chap09/code/pMinusOne.R
@@ -7,8 +7,14 @@ pMinusOne <- function(
 
 	require(schoolmath);
 
+	temp.gcd <- NULL;
 	for (b in 1:num.bases.to.try) {
-		A.to.i = sample(x = seq(2,n-1), size = 1);
+		
+		#A.to.i   <- sample(x = seq(2,n-1), size = 1);
+		A.to.i   <- sample(x = seq(2,47), size = 1);
+		temp.gcd <- gcd(A.to.i,n);
+		if (1 < temp.gcd) { return(temp.gcd); }
+
 		for (i in seq(1:loop.bound)) {
 			A.to.i   <- RepeatedSquaring(base = A.to.i, exponent = i, modulus = n);
 			temp.gcd <- gcd(A.to.i - 1,n);
@@ -16,6 +22,7 @@ pMinusOne <- function(
 			if (1 < temp.gcd & temp.gcd < n){ return(temp.gcd); }
 			if (n == temp.gcd){ break; }
 			}
+
 		}
 
 	return(NULL);
