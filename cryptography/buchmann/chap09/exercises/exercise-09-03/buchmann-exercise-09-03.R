ef054960ca0e359c30a5c3262eb854b4199cd5f5
Kenneth Chu <paradisepilot@gmail.com>
2013-08-13 14:05:27 -0400

    updated: buchmann-exercise-09-03.R

--- cryptography/buchmann/chap09/exercises/exercise-09-03/buchmann-exercise-09-03.R
+++ cryptography/buchmann/chap09/exercises/exercise-09-03/buchmann-exercise-09-03.R
@@ -5,34 +5,24 @@ source("../../../code/RepeatedSquaring.R");
 source("../../../code/pMinusOne.R");
 
 ####################################################################################################
-n <- 138277151;
-pMinusOne(
-	n                = n,
-	#loop.bound       = 47 * 10,
-	#num.bases.to.try = 100
-	);
-
-q();
-
-####################################################################################################
-size     <- 1000;
-base     <- sample(size = size, x = seq( 2,10), replace = TRUE);
-exponent <- sample(size = size, x = seq( 5, 8), replace = TRUE);
-modulus  <- sample(size = size, x = seq(17,51), replace = TRUE);
-
-correct.remainders  <- (base ^ exponent) %% modulus;
-computed.remainders <- integer(length = size);
-for (i in 1:size) {
-	computed.remainders[i] <- RepeatedSquaring(
-		base     = base[i],
-		exponent = exponent[i],
-		modulus  = modulus[i]
-		);
-	}
-
-sum(abs(computed.remainders-correct.remainders));
-all.equal(computed.remainders,correct.remainders);
-cbind(n = (base ^ exponent), modulus, computed.remainders, correct.remainders);
+#size     <- 1000;
+#base     <- sample(size = size, x = seq( 2,10), replace = TRUE);
+#exponent <- sample(size = size, x = seq( 5, 8), replace = TRUE);
+#modulus  <- sample(size = size, x = seq(17,51), replace = TRUE);
+#
+#correct.remainders  <- (base ^ exponent) %% modulus;
+#computed.remainders <- integer(length = size);
+#for (i in 1:size) {
+#	computed.remainders[i] <- RepeatedSquaring(
+#		base     = base[i],
+#		exponent = exponent[i],
+#		modulus  = modulus[i]
+#		);
+#	}
+#
+#sum(abs(computed.remainders-correct.remainders));
+#all.equal(computed.remainders,correct.remainders);
+#cbind(n = (base ^ exponent), modulus, computed.remainders, correct.remainders);
 
 ####################################################################################################
 n <- 138277151;
@@ -84,4 +74,14 @@ print( results$exponents );
 print( results$unfactored );
 
 results$unfactored * prod(results$prime.factors ^ results$exponents);
+
+####################################################################################################
+#n <- 138277151;
+#n <- (3^2)*(11*5);
+n <- 521*11617;
+pMinusOne(
+	n                = n,
+	loop.bound       = 30,
+	#num.bases.to.try = 100
+	);
 
