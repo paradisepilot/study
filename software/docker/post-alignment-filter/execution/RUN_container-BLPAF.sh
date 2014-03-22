efeb6f1a43ff191b2c6a9f673e5ffac9763f08fc
paradisepilot <paradisepilot@gmail.com>
2014-03-22 12:12:02 -0400

    updated: RUN_container-BLPAF.sh

--- software/docker/post-alignment-filter/execution/RUN_container-BLPAF.sh
+++ software/docker/post-alignment-filter/execution/RUN_container-BLPAF.sh
@@ -6,7 +6,8 @@ inputSAM=/data/test-data-100K.sam
 post_alignment_filtered_SAM=/output/output.sam
 logFile=/output/log.BLPAF
 
-docker run -i -t -v ${launchDIR}/data:/data:ro -v ${launchDIR}/output:/output BLPAFcontainer /bin/bash
+#docker run -i -t -v ${launchDIR}/data:/data:ro -v ${launchDIR}/output:/output BLPAFcontainer /bin/bash
+docker run -i -t -v ./chris/data:/data:ro -v ./chris/output:/output BLPAFcontainer /bin/bash
 
 #docker run -i -t -v ${launchDIR}/data:/data:ro -v ${launchDIR}/output:/output:rw BLPAFcontainer --inputSAM ${inputSAM} --outputSAM ${post_alignment_filtered_SAM} --log ${logFile}
 
