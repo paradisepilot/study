29dce8d300b0e0ef3c7de74e483ffd8045a2178b
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-10-06 15:30:36 -0400

    updated: main.R

--- earth-observation/synthetic-aperture-radar/005-fpca-analysis/code/main.R
+++ earth-observation/synthetic-aperture-radar/005-fpca-analysis/code/main.R
@@ -48,7 +48,7 @@ test_getData_one.variable_elongate();
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 temp.dir   <- gsub(x = output.directory, pattern = "005-fpca-analysis.+", replacement = "");
-temp.dir   <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW/output.AAW.kc-512.2021-10-04.01");
+temp.dir   <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-04.01");
 temp.file  <- "coregistered_stack.nc";
 ncdf4.snap <- file.path(temp.dir,temp.file)
 
