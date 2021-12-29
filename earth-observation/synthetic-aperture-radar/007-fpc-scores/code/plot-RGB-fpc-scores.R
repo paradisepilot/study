423bad6c336e59546b5f8eeabfd0177a52f26cca
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-12-29 02:40:13 -0500

    updated: plot-RGB-fpc-scores.R

--- earth-observation/synthetic-aperture-radar/007-fpc-scores/code/plot-RGB-fpc-scores.R
+++ earth-observation/synthetic-aperture-radar/007-fpc-scores/code/plot-RGB-fpc-scores.R
@@ -111,7 +111,7 @@ plot.RGB.fpc.scores_terrainr <- function(
         width    = 16,
         height   = 16 * (range.y/range.x),
         units    = "in",
-        dpi      = 600
+        dpi      = 300
         );
 
     remove(list = c('DF.temp','my.ggplot','range.lat','range.lon'));
