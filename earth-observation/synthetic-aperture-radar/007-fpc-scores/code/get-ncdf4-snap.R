51b6af2a052017f3975c35fcdacc2ffcf21b63cc
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-12-29 08:36:18 -0500

    updated: get-ncdf4-snap.R

--- earth-observation/synthetic-aperture-radar/007-fpc-scores/code/get-ncdf4-snap.R
+++ earth-observation/synthetic-aperture-radar/007-fpc-scores/code/get-ncdf4-snap.R
@@ -11,11 +11,12 @@ get.ncdf4.snap <- function(
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     if ( "bay-of-quinte" == study.area ) {
         temp.dir   <- gsub(x = output.directory, pattern = "007-fpc-scores.+", replacement = "");
-        #temp.dir  <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-04.01");
+        # temp.dir <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-04.01");
         temp.dir   <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-07.01.coreg.only");
     } else if ( "drummondville" == study.area ) {
         temp.dir   <- gsub(x = output.directory, pattern = "007-fpc-scores.+", replacement = "");
-        temp.dir   <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-25.01");
+        # temp.dir <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-25.01");
+        temp.dir   <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-28.01");
     } else {
         error.message <- paste0("\nunrecognized study area: ",study.area,"\n");
         stop( error.message );
