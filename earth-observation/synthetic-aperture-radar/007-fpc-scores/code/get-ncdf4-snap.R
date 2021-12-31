d0638e618d320855852b34d00b80a4c1941aef65
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-12-31 14:50:02 -0500

    updated: get-ncdf4-snap.R

--- earth-observation/synthetic-aperture-radar/007-fpc-scores/code/get-ncdf4-snap.R
+++ earth-observation/synthetic-aperture-radar/007-fpc-scores/code/get-ncdf4-snap.R
@@ -9,12 +9,17 @@ get.ncdf4.snap <- function(
     cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    task.name <- getwd();
+    task.name <- gsub(x = task.name, pattern = ".+earth-observation.synthetic-aperture-radar.", replacement ="");
+    task.name <- gsub(x = task.name, pattern = ".output", replacement = "");;
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     if ( "bay-of-quinte" == study.area ) {
-        temp.dir   <- gsub(x = output.directory, pattern = "007-fpc-scores.+", replacement = "");
+        temp.dir   <- gsub(x = output.directory, pattern = paste0(task.name,".+"), replacement = "");
         # temp.dir <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-04.01");
         temp.dir   <- file.path(temp.dir,"004-preprocess","02-bay-of-quinte","01-AAW","output.AAW.kc-512.2021-10-07.01.coreg.only");
     } else if ( "drummondville" == study.area ) {
-        temp.dir   <- gsub(x = output.directory, pattern = "007-fpc-scores.+", replacement = "");
+        temp.dir   <- gsub(x = output.directory, pattern = paste0(task.name,".+"), replacement = "");
         # temp.dir <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-25.01");
         temp.dir   <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-29.01");
         # temp.dir <- file.path(temp.dir,"004-preprocess","04-drummondville","01-AAW","output.AAW.kc-512.2021-12-29.02");
