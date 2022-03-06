ca0e844229d57d703bb76f5530e58fc78cc60fbe
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-03-06 10:52:16 -0500

    added: 001-vPIC-test/

---
+++ software/vPIC-USDOT-VIN-decoder/001-vPIC-test/code/main.R
@@ -0,0 +1,82 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+
+print( data.directory );
+print( code.directory );
+print( output.directory );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+require(odbc);
+require(DBI);
+
+# source supporting R code
+code.files <- c(
+    # "initializePlot.R",
+    );
+
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+my.seed <- 7654321;
+set.seed(my.seed);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+my.connection <- odbc::dbConnect(
+    drv      = odbc::odbc(),
+    Driver   = "ODBC Driver for MS SQL",     # defined in /usr/local/etc/odbcinst.ini
+    Server   = "Kenneths-MacBook-Pro.local", # mssql> SELECT HOST_NAME()
+    Database = "vPICList",
+    UID      = "SA",
+    PWD      = "Dummy20031230Password",
+    Port     = 1433
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+vin.17.s  <- c('1GCPCPC03BW4JTCSZ','1GCJC89U577PANEAH','1GTR2VC03B4V25VG5','1GTHC79U51HZF8UCA');
+vin.11.s  <- as.character(sapply(X = vin.17.s, FUN = function(x) { return(substr(x,1,11)) } ));
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+temp.vins <- c(vin.11.s,vin.17.s);
+for (temp.vin in temp.vins) {
+    my.results <- odbc::dbGetQuery(
+        conn      = my.connection,
+        statement = paste0("USE vPICList; EXEC [dbo].[spVinDecode] @v = N'",temp.vin,"'")
+        );
+    write.csv(
+        file      = paste0(temp.vin,".csv"),
+        x         = my.results,
+        row.names = FALSE
+        );
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+DBI::dbDisconnect(conn = my.connection);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
---
+++ software/vPIC-USDOT-VIN-decoder/001-vPIC-test/run-main.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# data_snapshot=NULL
+#
+# for argument in "$@"
+# do
+#     key=$(  echo $argument | cut -f1 -d=)
+#     value=$(echo $argument | cut -f2 -d=)
+#
+#     if [[ $key == *"--"* ]]; then
+#         v="${key/--/}"
+#         declare $v="${value}"
+#    fi
+# done
+#
+# args=()
+# args+=( '--data_snapshot' ${data_snapshot} )
+
+##################################################
+currentDIR=`pwd`
+   codeDIR=${currentDIR}/code
+ outputDIR=${currentDIR//github/gittmp}/output
+
+parentDIR=`dirname ${currentDIR}`
+  dataDIR=${parentDIR}/000-data
+
+if [ ! -d ${outputDIR} ]; then
+	mkdir -p ${outputDIR}
+fi
+
+cp -r ${codeDIR} ${outputDIR}
+cp    $0         ${outputDIR}/code
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### Docker Desktop is assumed installed.
+
+### Pull MS SQL Server docker image from Microsoft's Docker Hub registry
+# docker pull mcr.microsoft.com/mssql/server
+
+### Launch MS SQL Server container
+# docker run -d --name containerName -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Dummy20031230Password' -p 1433:1433 mcr.microsoft.com/mssql/server
+
+### Copy vPIC MS SQL backup file from host file system to Docker container file system:
+# docker cp <path to the file vPICList_lite_2022_02.bak in host file system> containerName:/var/opt/mssql/data/vPICList_lite_2022_02.bak
+# docker cp ${dataDIR}/vPIC/2022-02/vPICList_lite_2022_02.bak                containerName:/var/opt/mssql/data/vPICList_lite_2022_02.bak
+
+# sleep 10
+
+##################################################
+myRscript=${codeDIR}/main.R
+stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
+stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
+R --no-save --args ${dataDIR} ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+
+##################################################
+### Kill vPIC Docker containter
+# sleep 10
+# docker kill containerName
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+exit
