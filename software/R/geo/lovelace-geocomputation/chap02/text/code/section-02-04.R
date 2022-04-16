391bcc0a7c4e2d21e0d011c14cbba805e843c334
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-04-16 16:37:01 -0400

    updated: section-02-04.R

--- software/R/geo/lovelace-geocomputation/chap02/text/code/section-02-04.R
+++ software/R/geo/lovelace-geocomputation/chap02/text/code/section-02-04.R
@@ -26,6 +26,44 @@ section.02.04 <- function(
     print( rgdal::projInfo(type = 'units')   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.epsg <- rgdal::make_EPSG();
+
+    cat("\nstr(DF.epsg)\n");
+    print( str(DF.epsg)   );
+
+    cat("\nDF.epsg[1:10,]\n");
+    print( DF.epsg[1:10,]   );
+
+    cat("\nDF.epsg[DF.epsg[,'code'] == 4326,]\n");
+    print( DF.epsg[DF.epsg[,'code'] == 4326,]   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    vector.object <- sf::st_read(
+        dsn = system.file("vector/zion.gpkg", package = "spDataLarge")
+        );
+
+    cat("\nst_crs(vector.object)\n");
+    print( st_crs(vector.object)   );
+
+    vector.object <- sf::st_set_crs(x = vector.object, value = 4326); # WARNING: replacing CRS does not reproject data; use st_transform instead.
+
+    cat("\nst_crs(vector.object)\n");
+    print( st_crs(vector.object)   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    raster.filepath <- system.file(file.path("raster","srtm.tif"), package = "spDataLarge");
+    raster.object   <- raster::raster(raster.filepath);
+
+    cat("\nraster::projection(raster.object)\n");
+    print( raster::projection(raster.object)   );
+
+    cat("\ngrep(x = DF.epsg[,'prj4'], pattern = 'utm.+zone=12.+GRS80.+no_defs', value = TRUE)\n");
+    print( grep(x = DF.epsg[,'prj4'], pattern = 'utm.+zone=12.+GRS80.+no_defs', value = TRUE)   );
+
+    raster::projection(raster.object) <- "+proj=utm +zone=12 +ellps=GRS80 +units=m +no_defs +type=crs"
+
+    cat("\nraster::projection(raster.object)\n");
+    print( raster::projection(raster.object)   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat(paste0("\n",thisFunctionName,"() quits."));
