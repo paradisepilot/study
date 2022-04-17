53e88281964e592b859b70a1c0634e7b84669146
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-04-17 09:57:33 -0400

    updated: main.R section-03-03.R

--- software/R/geo/lovelace-geocomputation/chap03/text/code/section-03-03.R
+++ software/R/geo/lovelace-geocomputation/chap03/text/code/section-03-03.R
@@ -21,7 +21,7 @@ section.03.03 <- function(
         xmx   =  1.5,
         ymn   = -1.5,
         ymx   =  1.5,
-        vals  = 1:36
+        vals  = rnorm(n = 36)
         );
 
     cat("\nstr(elevation.raster)\n");
@@ -94,72 +94,20 @@ section.03.03 <- function(
     print( raster::cellStats(x = my.raster.stack, stat = summary)   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # raster.filepath <- system.file(file.path("raster","srtm.tif"), package = "spDataLarge");
-    # raster.object   <- raster::raster(raster.filepath);
-    #
-    # cat("\nraster.object\n");
-    # print( raster.object   );
-    #
-    # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # png.output <- "figure-02-12.png";
-    # png(filename = png.output, width = 16, height = 8, units = "in", res = 300, bg = "white");
-    # plot(raster.object);
-    # dev.off();
-    #
-    # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # cat("\nraster::writeFormats()\n");
-    # print( raster::writeFormats()   );
-    #
-    # cat("\nrgdal::gdalDrivers()\n");
-    # print( rgdal::gdalDrivers()   );
-    #
-    # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # raster.object.2 <- raster::raster(
-    #     nrow =  6,
-    #     ncol =  6,
-    #     res  =  0.5,
-    #     xmn  = -1.5,
-    #     xmx  =  1.5,
-    #     ymn  = -1.5,
-    #     ymx  =  1.5,
-    #     vals = 1:36
-    #     );
-    #
-    # cat("\nraster.object.2\n");
-    # print( raster.object.2   );
-    #
-    # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # brick.landsat <- raster::brick(
-    #     x = system.file(file.path("raster","landsat.tif"), package = "spDataLarge")
-    #     );
-    #
-    # cat("\nbrick.landsat\n");
-    # print( brick.landsat   );
-    #
-    # cat("\nnlayers(brick.landsat)\n");
-    # print( nlayers(brick.landsat)   );
-    #
-    # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # raster.layer.1 <- raster::raster(x = brick.landsat, layer = 1);
-    # raster.layer.2 <- raster::raster(
-    #     xmn        = raster.layer.1@extent@xmin,
-    #     xmx        = raster.layer.1@extent@xmax,
-    #     ymn        = raster.layer.1@extent@ymin,
-    #     ymx        = raster.layer.1@extent@ymax,
-    #     resolution = raster::res(raster.layer.1)
-    #     );
-    # crs(raster.layer.2) <- crs(raster.layer.1);
-    # values(raster.layer.2) <- sample(
-    #     base::seq_len(raster::ncell(raster.layer.2))
-    #     );
-    #
-    # my.sf.stack <- raster::stack(
-    #     raster.layer.1,
-    #     raster.layer.2
-    #     );
-    #
-    # cat("\nmy.sf.stack\n");
-    # print( my.sf.stack   );
+    png.output <- "figure-elevation-histogram.png";
+    png(filename = png.output, width = 16, height = 8, units = "in", res = 300, bg = "white");
+    hist(my.raster.stack[['elevation']]);
+    dev.off();
+
+    png.output <- "figure-elevation-density.png";
+    png(filename = png.output, width = 16, height = 8, units = "in", res = 300, bg = "white");
+    density(my.raster.stack[['elevation']]);
+    dev.off();
+
+    png.output <- "figure-elevation-boxplot.png";
+    png(filename = png.output, width = 16, height = 8, units = "in", res = 300, bg = "white");
+    boxplot(my.raster.stack[['elevation']]);
+    dev.off();
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat(paste0("\n",thisFunctionName,"() quits."));
