0148dfbbc86082eab34ca6509a85b0427934f432
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-11-11 13:10:53 -0500

    updated: test-conda_clone-install_ee.R test-conda_create.R

--- software/R/reticulate/code/test-conda_clone-install_ee.R
+++ software/R/reticulate/code/test-conda_clone-install_ee.R
@@ -12,30 +12,37 @@ test.conda_clone.install_ee <- function(
     require(reticulate);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nclone.path\n");
+    print( clone.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     if ( dir.exists(clone.path) ) {
+        cat("\nThe conda environment '",clone.path,"' already exists; activating this conda environment ...\n");
         reticulate::use_condaenv(condaenv = clone.path);
+        cat("\nThe conda environment '",clone.path,"' has been activated ...\n");
         my.python.path <- reticulate::py_config()[['python']];
     } else {
+        cat("\nThe conda environment '",clone.path,"' does not yet exist; cloning the environment '",to.be.cloned,"' ...\n");
+        cat("\nConda environment cloning begins: '",to.be.cloned,"' --> '",clone.path,"'\n");
         my.python.path <- reticulate::conda_clone(
             envname = clone.path,
             clone   = to.be.cloned
             );
+        cat("\nConda environment cloning complete: '",to.be.cloned,"' --> '",clone.path,"'\n");
+        cat("\nInstallation of earthengine-api begin: '",clone.path,"'\n");
         reticulate::conda_install(
             envname  = clone.path,
             packages = c("earthengine-api"),
             forge    = TRUE,
             conda    = "auto"
             );
+        cat("\nInstallation of earthengine-api complete: '",clone.path,"'\n");
         }
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat("\nmy.python.path\n");
     print( my.python.path   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    cat("\nclone.path\n");
-    print( clone.path   );
-
     cat("\nreticulate::py_list_packages(envname = clone.path, type = 'conda')\n");
     print(
         reticulate::py_list_packages(
--- software/R/reticulate/code/test-conda_create.R
+++ software/R/reticulate/code/test-conda_create.R
@@ -11,30 +11,34 @@ test.conda_create <- function(
     require(reticulate);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nenv.path\n");
+    print( env.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     if ( dir.exists(env.path) ) {
+        cat("\nThe conda environment '",env.path,"' already exists; activating this conda environment ...\n");
         # got the following error when trying to use the conda environment created with conda_create
         # use_condaenv(condaenv = "condaEnvGEE")
         # Error: 'condaEnvGEE/bin/python' was not built with a shared library.
         # reticulate can only bind to copies of Python built with '--enable-shared'.
         reticulate::use_condaenv(condaenv = env.path);
+        cat("\nThe conda environment '",env.path,"' has been activated ...\n");
         my.python.path <- reticulate::py_config()[['python']];
     } else {
+        cat("\nConda environment creation begins: '",env.path,"'\n");
         my.python.path <- reticulate::conda_create(
             envname  = env.path,
             packages = c("earthengine-api"),
             forge    = TRUE,
             conda    = "auto"
             );
+        cat("\nConda environment creation complete: '",env.path,"'\n");
         }
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat("\nmy.python.path\n");
     print( my.python.path   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    cat("\nenv.path\n");
-    print( env.path   );
-
     cat("\nreticulate::py_list_packages(envname = env.path, type = 'conda')\n");
     print(
         reticulate::py_list_packages(
