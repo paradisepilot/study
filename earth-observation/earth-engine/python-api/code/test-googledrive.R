fb40b47d9e121e767aedab145be66493b3630f50
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-12-08 18:04:39 -0500

    added: main.R test-googledrive.R

---
+++ earth-observation/earth-engine/python-api/code/test-googledrive.R
@@ -0,0 +1,46 @@
+
+test.googledrive <- function(
+    google.drive.folder = "earthengine"
+    ) {
+
+    thisFunctionName <- "test.googledrive";
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    options(
+        gargle_oauth_email = Sys.getenv("GARGLE_OAUTH_EMAIL"),
+        gargle_oauth_cache = Sys.getenv("GARGLE_OAUTH_CACHE")
+        );
+
+    require(googledrive);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.drive <- as.data.frame(googledrive::drive_find());
+
+    cat("\nDF.drive[,c('name','id')]\n");
+    print( DF.drive[,c('name','id')]   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.earth.engine <- as.data.frame(googledrive::drive_ls(
+        path    = google.drive.folder,
+        pattern = "\\.tif"
+        ));
+
+    cat("\nDF.earth.engine\n");
+    print( DF.earth.engine   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    for ( temp.id in DF.earth.engine[,'id'] ) {
+        cat("\ndownloading:",temp.id,"\n");
+        googledrive::drive_download(file = googledrive::as_id(temp.id));
+        }
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n",thisFunctionName,"() quits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( NULL );
+
+    }
+
+##################################################
