fdbd87067a41660c3888322d93165af1517c8844
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-12-08 17:22:37 -0500

    updated: main.py test_eeBatchExport.py

--- earth-observation/earth-engine/python-api/code/main.py
+++ earth-observation/earth-engine/python-api/code/main.py
@@ -39,111 +39,11 @@ logging.basicConfig(filename='log.debug',level=logging.DEBUG)
 from test_eeAuthenticate import test_eeAuthenticate
 from test_eeBatchExport  import test_eeBatchExport
 
-# import numpy   as np
-# import pandas  as pd
-#
-# from examineData       import examineData
-# from splitTrainTest    import splitTrainTest
-# from trainEvaluate     import trainEvaluate
-# from trainEvaluateGrid import trainEvaluateGrid
-# from visualizeData     import visualizeData
-#
-# from PipelinePreprocessHousingData import PipelinePreprocessHousingData
-#
-# from sklearn.tree            import DecisionTreeRegressor
-# from sklearn.linear_model    import LinearRegression
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.ensemble        import RandomForestRegressor
-
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 test_eeAuthenticate()
 test_eeBatchExport()
 
 # ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# # load data
-# housingFILE = os.path.join(dir_data,'housing.csv')
-# housingDF   = pd.read_csv(housingFILE);
-#
-# # examine full data set
-# examineData(inputDF = housingDF);
-#
-# ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# # split into training and testing data sets in a stratified manner
-# trainSet, testSet = splitTrainTest(inputDF = housingDF, random_state = 19)
-#
-# # visualize stratified training data set
-# visualizeData(inputDF = trainSet);
-#
-# print("\ntrainSet.info()")
-# print(   trainSet.info() )
-#
-# print("\ntrainSet.describe()")
-# print(   trainSet.describe() )
-#
-# print("\ntype(trainSet)")
-# print(   type(trainSet) )
-#
-# ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# # preprocess stratified training data set
-# preprocessedTrainSet = PipelinePreprocessHousingData.fit_transform(
-#     X = trainSet.drop(["median_house_value"],axis=1)
-#     )
-#
-# ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# # linear model (this underfits the data)
-# myLinearModel = LinearRegression()
-# trainEvaluate(
-#     trainData           = trainSet,
-#     testData            = testSet,
-#     trainedPreprocessor = PipelinePreprocessHousingData,
-#     myModel             = myLinearModel,
-#     modelName           = "Linear Model"
-#     )
-#
-# # regression tree (this overfits the data: zero MSE)
-# myRegressionTreeModel = DecisionTreeRegressor()
-# trainEvaluate(
-#     trainData           = trainSet,
-#     testData            = testSet,
-#     trainedPreprocessor = PipelinePreprocessHousingData,
-#     myModel             = myRegressionTreeModel,
-#     modelName           = "Regression Tree"
-#     )
-#
-# # random forest
-# myRandomForestModel = RandomForestRegressor()
-# trainEvaluate(
-#     trainData           = trainSet,
-#     testData            = testSet,
-#     trainedPreprocessor = PipelinePreprocessHousingData,
-#     myModel             = myRandomForestModel,
-#     modelName           = "Random Forest"
-#     )
-#
-# ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# # random forest with hyperparameter tuning via grid search
-# newRandomForestModel = RandomForestRegressor()
-#
-# parameterGrid = [
-#     { 'n_estimators':[3,10,30], 'max_features':[2,4,6,8]                     },
-#     { 'n_estimators':[3,10],    'max_features':[2,3,4],  'bootstrap':[False] }
-#     ]
-#
-# gridSearch = GridSearchCV(
-#     estimator  = newRandomForestModel,
-#     param_grid = parameterGrid,
-#     scoring    = "neg_mean_squared_error",
-#     cv         = 5
-#     )
-#
-# trainEvaluateGrid(
-#     trainData           = trainSet,
-#     testData            = testSet,
-#     trainedPreprocessor = PipelinePreprocessHousingData,
-#     myModel             = gridSearch,
-#     modelName           = "Random Forest, Cross Validation, Grid Search"
-#     )
 
 ##################################################
 ##################################################
