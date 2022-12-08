4d1e7f6a21fa1f4f9b2c2ffd4d88ce4774ece275
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-12-08 16:06:11 -0500

    updated: test_eeBatchExport.py

--- earth-observation/earth-engine/python-api/code/test_eeBatchExport.py
+++ earth-observation/earth-engine/python-api/code/test_eeBatchExport.py
@@ -9,35 +9,54 @@ def test_eeBatchExport():
     print( "\n### " + thisFunctionName + "() starts ..." )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    s2 = ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-    geometry = ee.Geometry.Polygon([[
+    my_point = ee.Geometry.Point(13.481643640792527,52.48959983479137);
+
+    s2 = ee.ImageCollection('COPERNICUS/S2_HARMONIZED') \
+        .filterDate(ee.Date('2019-05-01'),ee.Date('2019-08-01')) \
+        .filterBounds(my_point);
+
+    n_images = s2.size().getInfo();
+    print("\nn_images (s2)");
+    print(   n_images      );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    my_polygon = ee.Geometry.Polygon([[
       [82.60642647743225, 27.163504378052510],
       [82.60984897613525, 27.161852990137700],
       [82.61088967323303, 27.163695288375266],
       [82.60757446289062, 27.165174832309270]
     ]])
 
-    filtered = s2 \
+    filtered = ee.ImageCollection('COPERNICUS/S2_HARMONIZED') \
         .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',30)) \
         .filter(ee.Filter.date('2019-02-01','2019-03-01')) \
-        .filter(ee.Filter.bounds(geometry))
+        .filter(ee.Filter.bounds(my_polygon)) \
+        .map(maskS2clouds)
 
-    withNdvi = filtered \
-        .map(maskS2clouds) \
-        .map(addNDVI)
+    n_images = filtered.size().getInfo()
+    print("\nn_images (filtered)")
+    print(   n_images            )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    composite = withNdvi.median()
-    ndvi = composite.select('ndvi')
+    withNdvi = filtered.map(addNDVI)
+
+    n_images <- withNdvi.size().getInfo();
+    print("\nn_images (withNdvi)");
+    print(  n_images             );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ndvi = withNdvi.median().select('ndvi')
 
     stats = ndvi.reduceRegion(**{
       'reducer'   : ee.Reducer.mean(),
-      'geometry'  : geometry,
+      'geometry'  : my_polygon,
       'scale'     : 10,
       'maxPixels' : 1e10
       })
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    print("\nstats.getInfo()");
+    print(   stats.getInfo() );
+
     print("\nstats.get('ndvi').getInfo()\n")
     print(   stats.get('ndvi').getInfo()   )
 
@@ -48,7 +67,7 @@ def test_eeBatchExport():
 ##### ##### ##### ##### #####
 def maskS2clouds(image):
   qa = image.select('QA60')
-  cloudBitMask = 1 << 10
+  cloudBitMask  = 1 << 10
   cirrusBitMask = 1 << 11
   mask = qa.bitwiseAnd(cloudBitMask).eq(0).And(
              qa.bitwiseAnd(cirrusBitMask).eq(0))
